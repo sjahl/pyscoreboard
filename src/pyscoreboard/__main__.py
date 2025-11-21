6b129d4a65150080c0d7383f74d8436543cf9832
sjahl <636687+sjahl@users.noreply.github.com>
2025-11-20 20:56:07 -0500

    scaffold the cli and project

---
+++ src/pyscoreboard/__main__.py
@@ -0,0 +1,4 @@
+from pyscoreboard import app
+
+if __name__ == "__main__":
+    app.run()
---
+++ src/pyscoreboard/app.py
@@ -0,0 +1,68 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s",
+        "--sport",
+        help="Fetch scores for the specified sport",
+        type=str,
+        default="soccer",
+    )
+
+    parser.add_argument(
+        "-l",
+        "--league",
+        help="The league to fetch scores for",
+        type=str,
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def default_league(sport):
+    default_leagues = {
+        "soccer": "eng.1",
+        "baseball": "mlb",
+        "hockey": "nhl",
+        "football": "nfl",
+        "basketball": "nba",
+    }
+    return default_leagues[sport]
+
+
+def friendly_league_mapping(league):
+    league_aliases = {
+        "premier": "eng.1",
+        "championship": "eng.2",
+        "efl-1": "eng.3",
+        "efl-2": "eng.4",
+        "bundesliga": "ger.1",
+        "bundesliga-2": "ger.2",
+        "eredivisie": "ned.1",
+        "college-football": "cfb",
+        "mls": "usa.1",
+        "mx": "mex.1",
+        "laliga": "esp.1",
+        "ligue-1": "fra.1",
+    }
+
+    if league in league_aliases.keys():
+        return league_aliases[league]
+    else:
+        return league
+
+
+def run():
+    args = parse_args()
+    print("hello world")
+    print(f"You want to fetch scores for {args.sport}")
+
+    if not args.league:
+        lg = default_league(args.sport)
+    else:
+        lg = friendly_league_mapping(args.league)
+
+    print(f"You want to fetch scores for {lg}")
